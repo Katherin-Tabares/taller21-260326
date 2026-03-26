35a5015688790d07b4e552ac6a3f713a7d64024b
Katherin Samantha Tabares Castillo <est.katherin.tabares@unimilitar.edu.co>
2026-03-26 10:28:46 -0500

    Se coloco para ingresar un dato inicial, para terner la cantidad que se puede ingresar los datos del la listaEnlazada

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -1,7 +1,21 @@
 
+import java.util.Scanner;
+
 public class Taller21260326 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scn = new Scanner (System.in);
+        int n, m;
+        System.out.println("Ingrese un numero entero");
+        n=scn.nextInt();
+        ListaEnlazada lis = new ListaEnlazada();
+        for (int i=0; i<=n-1; i++){
+            m=scn.nextInt();
+            if (m>n){
+                i--;
+            }else{
+                lis.agregar(m);
+            }
+        }
     }
 }
