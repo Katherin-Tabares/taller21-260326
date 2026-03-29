644f7a28ec4d2b26194a50d82af09ac5066bae3f
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-03-29 16:16:59 -0500

    Muestra los numeros que estan en la lista, pero no muestra el numero que falta.

--- taller21-260326/src/main/java/ListaEnlazada.java
+++ taller21-260326/src/main/java/ListaEnlazada.java
@@ -24,6 +24,7 @@ public class ListaEnlazada {
             if(aux.getDato() == d){
                 return true;
             }
+            aux = aux.getSiguiente();
         }
         return false;
     }
