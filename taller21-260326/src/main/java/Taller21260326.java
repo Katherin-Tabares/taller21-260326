b120eb1dcf7ea2fdb75c2b61f3cae5217cab8350
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-03-29 16:48:59 -0500

    El trabajo finalizado, pero con indicaciones en la consola y en forma de comentario el codigo sin indicaciones.

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -30,5 +30,25 @@ public class Taller21260326 {
                 System.out.println("Numero faltante " + i);
             }
         }
+        //codigo en texto pero sin indicaciones
+        /*
+        n=scn.nextInt();
+        if (2<= n){
+            for (int i=0; i<n-1; i++){
+                m=scn.nextInt();
+                if (m<=n && !lis.numFaltante(m)){
+                    lis.agregar(m);
+                }else{
+                    i--;
+                }
+            }
+        }
+        System.out.println(lis);
+        for(int i=1; i<=n; i++){
+            if(!lis.numFaltante(i)){
+                System.out.println(i);
+            }
+        }
+        */
     }
 }
