51323d947b131b52a4ccb06716f22b946b4199a5
Katherin Tabares <est.katherin.tabares@unimilitar.edu.co>
2026-03-29 16:45:20 -0500

    El trabajo finalizado, pero con indicaciones en la consola.

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -9,33 +9,25 @@ public class Taller21260326 {
         
         Scanner scn = new Scanner (System.in);
         int n, m;
-        System.out.println("Ingrese un numero entero mayor de 2");
+        System.out.println("Ingrese un numero entero mayor o igual a 2");
         n=scn.nextInt();
         if (2<= n){
-            System.out.println("Ingresa los numeros de la lista");
+            System.out.println("Ingresa los numeros de la lista mayor que 1 y menor que " + n);
             for (int i=0; i<n-1; i++){
                 m=scn.nextInt();
-                
-                for(int j=1; j<=i; j++){
-                    if(!lis.numFaltante(j)){
-                        if (m<=n){
-                            lis.agregar(m);
-                        }else{
-                            i--;
-                        }
-                    }else{
-                        i--;
-                    }
-                }   
-                
-            }
-            System.out.println(lis); //muetra la lista
-            
-            //mostrar el faltante
-            for(int i=1; i<=n; i++){
-                if(!lis.numFaltante(i)){
-                System.out.println(i);
+                if (m<=n && !lis.numFaltante(m)){
+                    lis.agregar(m);
+                }else{
+                    System.out.println("Numero repetido o fuera de rango, ingrese otro");
+                    i--;
                 }
+            }
+        }
+        System.out.println(lis); //muetra la lista
+         //mostrar el faltante
+        for(int i=1; i<=n; i++){
+            if(!lis.numFaltante(i)){
+                System.out.println("Numero faltante " + i);
             }
         }
     }
